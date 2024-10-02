2687c11e2642e1093a115797d894e94feb8cc386
herjflorez <hernandojflorez2013@gmail.com>
2024-10-02 18:44:14 +0200

    conflictos intencionales multiple ambos

--- src/main/main.java
+++ src/main/main.java
@@ -9,9 +9,9 @@ public class main {
 		
 		for(int x = 1; x <= 100; x++) {
 			if(x%3 == 0 && x%5 != 0) {
-				System.out.println(x + " - Coca");
+				System.out.println(x + " - fanta");
 			} else if(x%5 == 0 && x%3 != 0) {
-				System.out.println(x + " - Cola");
+				System.out.println(x + " - sprite");
 			} else if(x%3 == 0 && x%5 == 0) {
 				System.out.println(x + " - FizzBuzz");
 			}
