1f17ac7cee6e1b82bb533e172731af5654d2f46e
Alejandro Martínez <alejandromartiinez79@gmail.com>
2024-10-02 18:26:58 +0200

    funcionalidad multiple 3 cambiada Fizz

--- src/main/main.java
+++ src/main/main.java
@@ -9,7 +9,7 @@ public class main {
 		
 		for(int x = 1; x <= 100; x++) {
 			if(x%3 == 0 && x%5 != 0) {
-				System.out.println(x + " - Coca");
+				System.out.println(x + " - Fizz");
 			} else if(x%5 == 0 && x%3 != 0) {
 				System.out.println(x + " - Cola");
 			} else if(x%3 == 0 && x%5 == 0) {
