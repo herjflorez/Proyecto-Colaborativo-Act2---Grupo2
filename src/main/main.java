b4678cbcbad2b1fcb5d6678bf154d60c5bcc830a
unknown <kilianra04@gmail.com>
2024-10-02 19:48:16 +0200

    conflicto forzado multiple de 3

--- src/main/main.java
+++ src/main/main.java
@@ -9,11 +9,11 @@ public class main {
 		
 		for(int x = 1; x <= 100; x++) {
 			if(x%3 == 0 && x%5 != 0) {
-				System.out.println(x + " - editado01");
+				System.out.println(x + " - Fizz");
 			} else if(x%5 == 0 && x%3 != 0) {
-				System.out.println(x + " - Buzz");
+				System.out.println(x + " - hola");
 			} else if(x%3 == 0 && x%5 == 0) {
-				System.out.println(x + " - FizzBuzz");
+				System.out.println(x + " - paratimicola");
       
 			}
 			else {
