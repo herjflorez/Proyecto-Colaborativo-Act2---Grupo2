0db7c2fa84151152006c8b1118f9fe27beb802c2
Alejandro Martínez <alejandromartiinez79@gmail.com>
2024-10-02 18:51:58 +0200

    conflicto forzado multiple 5

--- src/main/main.java
+++ src/main/main.java
@@ -9,11 +9,11 @@ public class main {
 		
 		for(int x = 1; x <= 100; x++) {
 			if(x%3 == 0 && x%5 != 0) {
-				System.out.println(x + " - Coca");
+				System.out.println(x + " - editado01");
 			} else if(x%5 == 0 && x%3 != 0) {
 				System.out.println(x + " - Buzz");
 			} else if(x%3 == 0 && x%5 == 0) {
-				System.out.println(x + " - CocaCola");
+				System.out.println(x + " - editado02");
 			}
 			else {
 				System.out.println(x);
