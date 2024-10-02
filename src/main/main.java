8fe91e7ae50146674392398faed4d747da690894
herjflorez <hernandojflorez2013@gmail.com>
2024-10-02 18:10:18 +0200

    application creation

--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,34 @@
 package main;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		main main = new main();
+		
+		
+		
+		for(int x = 1; x <= 100; x++) {
+			if(x%3 == 0 && x%5 != 0) {
+				System.out.println(x + " - Coca");
+			} else if(x%5 == 0 && x%3 != 0) {
+				System.out.println(x + " - Cola");
+			} else if(x%3 == 0 && x%5 == 0) {
+				System.out.println(x + " - CocaCola");
+			}
+			else {
+				System.out.println(x);
+			}
+		}
+		
+		
+		
+		
+//		main.ReadVal();
 
 	}
-
+	
 }
